d40ccdbcd370a6df8100c74bdb9abe5194926169
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-07-20 19:34:26 -0300

    agregue estados

--- src/TodoItem/TodoItem.js
+++ src/TodoItem/TodoItem.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './styles.css'
 
-function TodoItem({text, completed}){
+function TodoItem({text, completed, onCompleted, onDeleted}){
     return(
-      <li>
-        <span>V</span>
-        <p>{text}</p>
-        <span>X</span>
+      <li className={`${completed && "active"}`}>
+
+        <span onClick={onCompleted}>V</span>
+
+        <p className={`${completed? "through":"none"}`}>{text}</p>
+
+        <span onClick={onDeleted}>X</span>
+        
       </li>
     );
   }
