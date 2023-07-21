4fac16216e43e60489e4bf0203037ac4ef4e26dc
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-07-21 18:56:24 -0300

    quite las funciones

--- src/components/TodoViewList/index.js
+++ src/components/TodoViewList/index.js
@@ -1,18 +1,10 @@
 import React from 'react'
 import './style.css'
-import TodoBtnCreate from '../components/TodoBtnCreate/TodoBtnCreate'
+import TodoBtnCreate from '../TodoBtnCreate/index'
 import TodoTitle from '../TodoTitle'
 import TodoInput from '../TodoInput'
 import TodoList from '../TodoList'
 import TodoItem from '../TodoItem'
-import useLocalStorage from '../../utils/useLocalStorage'
-
-// const defaulTodos =[
-//     {text: 'Aprender React', completed: true},
-//     {text: 'Conocer librerias', completed: false},
-//     {text: 'Crear Proyectos', completed: false},
-//     {text:'Aprender React Native', completed: false}
-//   ];
 
 
 //TODO:agregar modal
@@ -24,42 +16,16 @@ import useLocalStorage from '../../utils/useLocalStorage'
 
 
 
-function TodoViewList() {
-  const [todos, saveTodos] = useLocalStorage('TODOS_v1',[]);//contiene todos los todos
-  const [inputState, setInputState] = React.useState('');//constiene el texto de los inputs
+function TodoViewList({
+  completedTodos,
+  totalTodos,
+  inputState,
+  setInputState,
+  searchedTodos,
+  completeTodo,
+  deleteTodo
+}) {
 
-
-  const completedTodos = todos.filter(todo => !!todo.completed).length;
-  const totalTodos = todos.length;
-
-    const searchedTodos = todos.filter(
-    (todo) => {
-      const todoText = todo.text.toLowerCase();
-      const searchText = inputState.toLowerCase();
-      return todoText.includes(searchText)
-    }
-  );
-
-
-
-  const completeTodo = (text) =>{//actualizador de todos a true
-    const newTodos = [...todos]
-    const todoIndex = newTodos.findIndex(
-      (todo)=> todo.text === text
-    );
-    newTodos[todoIndex].completed =true
-    saveTodos(newTodos)
-  };
-
-
-  const deleteTodo = (text) =>{//borrar todos
-    const newTodos = [...todos]
-    const todoIndex = newTodos.findIndex(
-      (todo)=> todo.text === text
-    );
-    newTodos.splice(todoIndex,1);
-    saveTodos(newTodos)
-  };
 
   return (
     <div className='view-list-container'>
