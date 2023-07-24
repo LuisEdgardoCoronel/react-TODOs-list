8e95c3c9db38854a20d318b08d17dc33b80ae009
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-07-24 20:34:45 -0300

    probando reactdom.createportal

--- src/components/TodoModal/TodoModal.js
+++ src/components/TodoModal/TodoModal.js
@@ -1,17 +1,23 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import './style.css'
-import TodoInput from '../TodoInput'
-import TodoBtnCreate from '../TodoBtnCreate'
+// import TodoInput from '../TodoInput'
+// import TodoBtnCreate from '../TodoBtnCreate'
 
-function TodoModal() {
-  return (
-    <div className='modal-container disabled'>
-      <h3>Ingrese una nueva tarea</h3>
-      <TodoInput/>
-      <TodoBtnCreate 
-      text={"Crear"}/>
-    </div>
+function TodoModal({childen}) {
+  return ReactDOM.createPortal(
+    <div className='modal-container enabled'>
+      {childen}
+    </div>, document.getElementById('modal')
   )
+  // return (
+  //   <div className='modal-container disabled'>
+  //     <h3>Ingrese una nueva tarea</h3>
+  //     <TodoInput/>
+  //     <TodoBtnCreate 
+  //     text={"Crear"}/>
+  //   </div>
+  // )
 }
 
 export default TodoModal
