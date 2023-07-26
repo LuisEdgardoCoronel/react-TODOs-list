a38cc4ec02642633e9ead288efc837070db0a1e3
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-07-25 23:58:04 -0300

    borre espacios

--- src/components/TodoContainer/index.js
+++ src/components/TodoContainer/index.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import './styles.css'
 import TodoViewList from '../TodoViewList'
-import TodoCreateDesktop from '../TodoCreateDesktop'
+import TodoCreateDesktop from '../TodoViewDesktop'
 import { TodoProvider } from '../../context'
 
 function TodoContainer() {
-
-  
-
-
-
-
 
   return (
     <div className='container'>
