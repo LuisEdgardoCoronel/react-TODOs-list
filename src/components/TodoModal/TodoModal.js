4ea6698f42d52be39c8fb9d6f90116126180759f
Luis Edgardo Coronel <lecoronel02@gmail.com>
2023-07-25 23:55:54 -0300

    creando el modal y ajustando props

--- src/components/TodoModal/TodoModal.js
+++ src/components/TodoModal/TodoModal.js
@@ -1,23 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom";
 import './style.css'
-// import TodoInput from '../TodoInput'
-// import TodoBtnCreate from '../TodoBtnCreate'
 
-function TodoModal({childen}) {
+function TodoModal({children}) {
+  const modalRoot = document.getElementById('modal')
   return ReactDOM.createPortal(
-    <div className='modal-container enabled'>
-      {childen}
-    </div>, document.getElementById('modal')
-  )
-  // return (
-  //   <div className='modal-container disabled'>
-  //     <h3>Ingrese una nueva tarea</h3>
-  //     <TodoInput/>
-  //     <TodoBtnCreate 
-  //     text={"Crear"}/>
-  //   </div>
-  // )
+    <div className='background-modal'>
+      <div className='modal'>{children}</div>
+    </div>, modalRoot
+  );
 }
 
 export default TodoModal
